26c1570cbd482b6b4bee3a87f962f1ef072af462
nanda yadav <nandayadav@gmail.com>
2010-03-10 00:51:54 -0700

    added mouseclicked/released events

--- twitter_visuals/viewer.rb
+++ twitter_visuals/viewer.rb
@@ -1,33 +1,46 @@
 require 'lib/twitter_api'
 
 class Viewer < Processing::App
-  #load_library :opengl
-  #include_package 'processing.opengl'
+  
   def setup
-    size 1000, 1000#, OPENGL
+    size 1000, 1000
     @results = []
     no_loop
   end
   
   def draw    
-    rect(10,10,20,20)
+    #rect(10,10,20,20)
     fetch_data
-    x, y = 0, 0
-    @results.each do |result|
+    x, y = mouse_x, mouse_y
+    @results.each_with_index do |result, index|
       image_url = result.profile_image_url
       b = loadImage(image_url)
-      x += 50
-      y += 50
-      link(result.source, "_new")
+      if index.even?
+        x += rand(80)
+        y += rand(80)
+      else
+        x -= rand(80)
+        y -= rand(80)
+      end
+      #link(result.source, "_new")
       image(b, x, y)
     end
   end
   
   def fetch_data
     client = TwitterClient::DataFetcher.new
-    @results = client.search("nepal")
+    terms = ['Nepal','Maradona','Kobe','Glen Beck','oscars']
+    @results = client.search(terms.sort_by{rand}.first)
+  end
+  
+  def mouse_pressed
+    loop
+  end
+  
+  def mouse_released
+    no_loop
   end
   
       
 end
-Viewer.new :title => "3D Visualization", :width => 1000, :height => 1000
+Viewer.new :title => "Twitter Search - Nepal", :width => 1000, :height => 1000
