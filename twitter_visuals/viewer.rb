f9b313edf65ba24322d2760952adbd5de7633616
nanda yadav <nandayadav@gmail.com>
2010-03-14 22:21:36 -0600

    added basic control panel with image filtering capability

--- twitter_visuals/viewer.rb
+++ twitter_visuals/viewer.rb
@@ -1,11 +1,34 @@
 require 'lib/twitter_api'
 
 class Viewer < Processing::App
-  
+  load_library :control_panel
   def setup
     size 1000, 1000
     @results = []
+    @rendered_images = []
+    control_panel do |c|
+      c.button :generate_stuff
+      c.menu(:options, ['none', 'blur', 'erode', 'gray','invert','opaque','dilate'], 'none') {|m| modify_image(m) }
+    end
     no_loop
+    @link = nil
+    @client = TwitterClient::DataFetcher.new
+    @terms = ['Nepal','Maradona','Kobe','Glen Beck','oscars']
+    @mode = nil
+  end
+  
+  def modify_image(mode)
+    if ['none','other'].include?(mode)
+      @mode = nil
+    else
+      @mode = mode.upcase 
+    end
+    redraw
+  end
+    
+  
+  def generate_stuff
+    redraw
   end
   
   def draw    
@@ -13,6 +36,7 @@ class Viewer < Processing::App
     fetch_data
     x, y = mouse_x, mouse_y
     @results.each_with_index do |result, index|
+      #sleep 1
       image_url = result.profile_image_url
       b = loadImage(image_url)
       if index.even?
@@ -22,15 +46,36 @@ class Viewer < Processing::App
         x -= rand(80)
         y -= rand(80)
       end
-      #link(result.source, "_new")
+      @link = result.source
+      puts "Link: #{@link}"
+      
+      @rendered_images << b
+      push_matrix
       image(b, x, y)
+      case @mode
+      when 'BLUR'
+        filter(BLUR, 2)
+      when 'ERODE'
+        filter(ERODE)
+      when 'DILATE'
+        filter(DILATE)
+      end
+      pop_matrix
     end
+    #blur_old_images
+  end
+  
+  def blur_old_images
+    push_matrix
+    @rendered_images.each do |img| 
+      filter(BLUR,2)
+    end
+    pop_matrix
   end
   
   def fetch_data
-    client = TwitterClient::DataFetcher.new
-    terms = ['Nepal','Maradona','Kobe','Glen Beck','oscars']
-    @results = client.search(terms.sort_by{rand}.first)
+    @results = []
+    @results = @client.search(@terms.sort_by{rand}.first)
   end
   
   def mouse_pressed
@@ -39,6 +84,7 @@ class Viewer < Processing::App
   
   def mouse_released
     no_loop
+    link(@link, "_new")
   end
   
       
