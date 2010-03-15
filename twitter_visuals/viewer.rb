1117d1b5997264eb8d30a1d5f75f34e2d5b34a0e
nanda yadav <nandayadav@gmail.com>
2010-03-14 23:18:29 -0600

    better image fading

--- twitter_visuals/viewer.rb
+++ twitter_visuals/viewer.rb
@@ -2,6 +2,7 @@ require 'lib/twitter_api'
 
 class Viewer < Processing::App
   load_library :control_panel
+  FADE_THRESHOLD = 3
   def setup
     size 1000, 1000
     @results = []
@@ -15,6 +16,8 @@ class Viewer < Processing::App
     @client = TwitterClient::DataFetcher.new
     @terms = ['Nepal','Maradona','Kobe','Glen Beck','oscars']
     @mode = nil
+    @bg_x, @bg_y, @bg_z = 100, 100, 100
+    background(@bg_x, @bg_y, @bg_z)
   end
   
   def modify_image(mode)
@@ -35,7 +38,7 @@ class Viewer < Processing::App
     #rect(10,10,20,20)
     fetch_data
     x, y = mouse_x, mouse_y
-    blur_old_images
+    filter_old_images
     @results.each_with_index do |result, index|
       #sleep 1
       image_url = result.profile_image_url
@@ -50,7 +53,7 @@ class Viewer < Processing::App
       @link = result.source
       puts "Link: #{@link}"
       
-      @rendered_images << {:image => b, :x => x, :y => y}
+      @rendered_images << {:image => b, :x => x, :y => y, :counter => 0}
       # push_matrix
       #       case @mode
       #       when 'BLUR'
@@ -66,17 +69,35 @@ class Viewer < Processing::App
     
   end
   
-  def blur_old_images
-    @rendered_images.each do |img| 
+  #Replace image pixels with background pixels
+  def fade_image(image)
+    img = image[:image]
+    x,y = image[:x], image[:y]
+    dimension = (img.width*img.height)
+    img.load_pixels
+    (0..dimension-1).each do |i|
+      img.pixels[i] = color(@bg_x, @bg_y, @bg_z)
+    end
+    img.update_pixels
+    image(img,x,y)
+  end
+    
+  #Apply various image filters
+  def filter_old_images
+    @rendered_images.each do |i| 
+      img = i[:image]
       case @mode
       when 'ERODE'
-        img[:image].filter(ERODE)
+        img.filter(ERODE)
       when 'DILATE'
-        img[:image].filter(DILATE)
+        img.filter(DILATE)
       else
-        img[:image].filter(BLUR,2)
+        img.filter(BLUR,4)
       end
-      image(img[:image], img[:x], img[:y])
+      #img.resize(img.width/2, img.height/2)
+      image(img, i[:x], i[:y])
+      i[:counter] += 1
+      fade_image(i) if i[:counter] > FADE_THRESHOLD
     end
   end
   
