7fd2e48f860c03109cb6054a3a99e32da0be34f4
nanda yadav <nandayadav@gmail.com>
2010-03-20 21:57:27 -0600

    took our unnecessary inst vars, uniform image sizes, proper visualiaztion with a centralized node

--- twitter_visuals/network_viewer.rb
+++ twitter_visuals/network_viewer.rb
@@ -1,23 +1,28 @@
 #Social Graph visualization(followers/friends) for specific twitter profile
 require 'lib/twitter_api'
-USER = 'nandayadav'
+USER = 'shovan'
 class NetworkViewer < Processing::App
   def setup
     @client = TwitterClient::DataFetcher.new
     no_loop
     @results = []
-    @height, @width = 800, 800
-    size @height, @width
+    size 800, 800
   end
   
   
   def draw
     fetch_data('friends')
+    x1, y1 = nil, nil
+    x_center = width/2
+    y_center = height/2
     @results.each do |result|
       image_url = result.profile_image_url
       b = loadImage(image_url)
-      x, y = rand(@width - 100), rand(@height - 100)
+      x, y = rand(width - 100), rand(height - 100)
+      #Smaller images, also to nullify sporadic 'big' profile images returned by twitter API
+      b.resize(40,40)
       image(b, x, y)
+      line(x_center, y_center, x, y)
     end
   end
   
